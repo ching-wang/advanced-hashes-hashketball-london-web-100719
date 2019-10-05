0135203506ae58bd416b05c2f43a7a359831dc9e
Qing Wang <chingwang.qw@gmail.com>
2019-10-05 18:45:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -252,7 +252,7 @@ def player_with_most_steals()
 end 
 
 def long_name_steals_a_ton?()
-  if player_with_longest_name() == player_with_most_steals()
+  if player_with_longest_name()[:name] == player_with_most_steals()[:name]
     return true
   else 
     return false 
